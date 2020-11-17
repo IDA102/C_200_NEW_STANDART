6493fb8c9907fa655fbbea3adebc1f7b14c8dbd7
IDA102 <60264142+IDA102@users.noreply.github.com>
2020-11-17 17:48:51 +0300

    FIX:4.2,~4,1

---
+++ LIB_4/H.h
@@ -0,0 +1,19 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <set>
+#include <list>
+#include <deque>
+#include <algorithm>
+#include <memory>
+#include <math.h>
+#include <iterator>
+#include <cstdint>
+#include <stdlib.h>
+#include <stack>
+
+#define NOP __asm nop
+using namespace std;
